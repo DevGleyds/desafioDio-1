e0a7edb066581b9ee5b0887ef69a039515239db6
DevGleyds <gleydsongui93@gmail.com>
2024-01-13 13:05:46 -0300

    commitFinal

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-console.log("Olá Mundo");
-console.log("ok");
+let xpFerro = 1000;
+let xpBronze = 2000;
+let xpPrata = 5000;
+let xpOuro = 7000;
+let xpPlatina = 8000;
+let xpAscendente = 9000;
+let xpImortal = 10000;
+let xpRadiante = 10001;
+
+let joãozinho = 8050;
+if (joãozinho <= xpFerro) console.log("Vc é Ferro");
+else if (joãozinho <= xpPrata) console.log("Você é Prata");
+else if (joãozinho <= xpOuro) console.log("Você é Ouro");
+else if (joãozinho <= xpPlatina) console.log("Você é platina");
+else if (joãozinho <= xpAscendente) console.log("Você é platina");
+else if (joãozinho <= xpImortal) console.log("Você é ascendente");
+else if (joãozinho <= xpRadiante) console.log("Você é radiante");
